8b694ef209b041be3538846afb16e18b94909ce5
TGRcoin <37862225+TGRcoin@users.noreply.github.com>
2018-04-13 19:33:36 +0700

    Add files via upload

--- NewToken.sol
+++ NewToken.sol
@@ -4,10 +4,10 @@ import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
 	function NewToken() {
-		totalSupply = 123;
-		name = "Best ever coin";
-		decimals = 4;
-		symbol = "BEC";
+		totalSupply = 2300000000000000;
+		name = "Tugarin";
+		decimals = 8;
+		symbol = "TGRN";
 		version = "1.0";
 		balances[msg.sender] = totalSupply;
 	}
